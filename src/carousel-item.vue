6533e5e85578f5b66fd3fc1330790648d1a96f36
lynchuh <lynchuh@gmail.com>
2018-11-08 14:48:15 +0800

    可以轮播（bug：往左轮播时图片会飘）

--- src/carousel-item.vue
+++ src/carousel-item.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="w-carousel-item" 
+    :class= "[{'current':index===currentCarousel}]"
     :style="itemStyle"
-    v-show="isOthers"
+    v-show="itemStyle"
    > 
   <slot></slot>
   </div>
+
 </template>
 <script>
 export default {
-  name: "WillCarouselItem",
+  name: 'WillCarouselItem',
   props: {
     index: {
       required: true,
@@ -17,40 +19,81 @@ export default {
   },
   data() {
     return {
-      itemStyle:{
-      },
-      width:0,
-      isOthers:true
-    };
+      itemStyle: null,
+      width: 0,
+      maxCarousel: 0,
+      currentCarousel: 0
+    }
   },
-  inject: ["eventBus"],
+  inject: ['eventBus'],
   mounted() {
-    this.eventBus.$on("carouselGoing", this.x);
-    this.width = this.$el.clientWidth
+    this.eventBus.$on('carouselGoing', this.changeCurrent)
   },
   created() {
+    this.eventBus.$on('initData', (maxCarousel, currentCarousel, width) => {
+      this.maxCarousel = maxCarousel
+      this.currentCarousel = currentCarousel
+      this.width = width
+      this.carouselGoing(this.currentCarousel)
+    })
   },
   methods: {
-    x(currentItem, maxItem) {
-      this.itemStyle = (currentItem === maxItem -1 && this.index ===0) ? {
-        transform: `translateX(${this.width}px)`
-      } : {
-        transform: `translateX(${this.width *(this.index - currentItem)}px)`
+    changeCurrent(currentCarousel) {
+      this.currentCarousel = currentCarousel
+    },
+    carouselGoing(value, oldValue) {
+      let copyStyle = {}
+      let multiple
+      if (this.index == value) {
+        console.log('new',this.index)
+        copyStyle['visibility'] = 'visible'
+      } else if (this.index == oldValue) {
+        console.log('old',this.index)
+        copyStyle['visibility'] = 'visible'
+      } else {
+        console.log('others',this.index)
       }
-      if(currentItem === 0){
 
-      } else if(currentItem === maxItem -1){
-        this.isOthers = (this.index === currentItem || this.index === 0 ) ? true :false
-      } else{
-        this.isOthers = ( (this.index === currentItem || this.index === 0 ) )
-      } 
+
+      switch (value) {
+        case 0:
+          if (this.index === 1) {
+            multiple = 1
+          } else if (this.index === 0) {
+            multiple = 0
+          } else {
+            multiple = this.index - this.maxCarousel
+          }
+          break
+        case this.maxCarousel - 1:
+          multiple = this.index === 0 ? 1 : this.index - value
+          break
+        default:
+          if (this.index === value + 1) {
+            multiple = 1
+          } else {
+            multiple =
+              this.index > value
+                ? this.index - value - this.maxCarousel
+                : this.index - value
+          }
+          break
+      }
+      // console.log('index:',this.index,'倍数', multiple)
+      copyStyle.transform = ` translateX(${this.width * multiple}px)`
+      this.itemStyle = copyStyle
+    }
+  },
+  watch: {
+    currentCarousel(value, oldValue) {
+      this.carouselGoing(value, oldValue)
     }
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .w-carousel-item {
   transition: all 0.5s;
-
+  visibility: hidden;
 }
 </style>
--- src/carousel.vue
+++ src/carousel.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="w-carousel">
-    <button type="button" class="w-carousel-arrow arrow-left" @click="ClickArrow($event,'pre')">
+    <button type="button" class="w-carousel-arrow arrow-left" @click="ClickArrow($event,'left')">
       <w-icon name="left"></w-icon>      
     </button>
     <div class="w-carousel-wrapper">
       <slot></slot>
     </div>
-    <button type="button" class="w-carousel-arrow arrow-right" @click="ClickArrow($event,'next')">
+    <button type="button" class="w-carousel-arrow arrow-right" @click="ClickArrow($event,'right')">
       <w-icon name="right"></w-icon>      
     </button>
     <ul>
-      <li class="w-indicator" v-if="maxCarousel" v-for= "(item , index) in maxCarousel" :key="item" @click="ClickIndicator($event,index)"></li>
+      <li class="w-indicator" :class = "{'active':index === currentCarousel}"
+       v-if="maxCarousel" v-for= "(item , index) in maxCarousel" :key="item" @click="ClickIndicator($event,index)"></li>
     </ul>
   </div>
 </template>
@@ -18,125 +19,129 @@
 import Icon from './icon.vue'
 import Vue from 'vue'
 export default {
-  components:{
-    'w-icon':Icon
+  components: {
+    'w-icon': Icon
   },
-  props:{
-    defaultCarousel:{
-      default:0,
-      type:[Number,String]
+  props: {
+    defaultCarousel: {
+      default: 0,
+      type: [Number, String]
     },
   },
   data() {
     return {
-      maxCarousel:0,
-      currentCarousel:0,
-      eventBus:new Vue()
-    };
+      maxCarousel: 0,
+      currentCarousel: 0,
+      width:0,
+      eventBus: new Vue()
+    }
   },
-  provide(){
+  provide() {
     return {
-      eventBus:this.eventBus
-      }
+      eventBus: this.eventBus
+    }
   },
-  created(){
-    this.currentCarousel = this.defaultCarousel  //init currentCarousel
+  created() {
+    this.currentCarousel = this.defaultCarousel //init currentCarousel
   },
   mounted() {
-    this.getMaxCarousel()  
+    this.getMaxCarousel() 
+    this.width = this.$el.clientWidth
+    this.eventBus.$emit('initData', this.maxCarousel,this.currentCarousel,this.width) 
   },
   methods: {
     getMaxCarousel() {
-      let num = 0;
+      let num = 0
       this.$children.map(item => {
-        item.$options.name === "WillCarouselItem" && num++;
-      });
-      this.maxCarousel = num;
+        item.$options.name === 'WillCarouselItem' && num++
+      })
+      this.maxCarousel = num
     },
-
-    ClickIndicator($event,index){
+    ClickIndicator($event, index) {
       this.currentCarousel = index
     },
-    ClickArrow($event,target){
-      if(target === 'pre'){
-
-        this.currentCarousel = this.currentCarousel === 0 ? this.maxCarousel-1 : this.currentCarousel - 1
-
-      }else if(target === 'next'){
-
-        this.currentCarousel = this.currentCarousel === this.maxCarousel-1 ? 0 : this.currentCarousel + 1
-
+    ClickArrow($event, target) {
+      if (target === 'left') {
+        this.currentCarousel = this.currentCarousel === 0 ? this.maxCarousel - 1 : this.currentCarousel - 1
+      } else if (target === 'right') {
+        this.currentCarousel =
+          this.currentCarousel === this.maxCarousel - 1
+            ? 0
+            : this.currentCarousel + 1
       }
-
     }
   },
-  watch:{
-    currentCarousel(newVal,oldVal){
-      console.log(newVal)
-      this.eventBus.$emit('carouselGoing',newVal,this.maxCarousel)
+  watch: {
+    currentCarousel(newVal, oldVal) {
+      // console.log('--------------')
+      // console.log(newVal)
+      this.eventBus.$emit('carouselGoing', newVal, oldVal)
     }
   }
-};
+}
 </script>
 <style lang="scss" scoped>
-  .w-carousel{
-    *{box-sizing: border-box}
-    // min-height:250px;
-    border:1px solid red;
-    position:relative;
-    // overflow: hidden;
-    .w-carousel-arrow{
-      border:none;
-      outline: none;
-      margin:0;
-      padding:.3em;  //去除默认样式
-      border-radius: 50%;
-      background-color:rgba(31,45,61,.11);
+.w-carousel {
+  * {
+    box-sizing: border-box;
+  }
+  // min-height:250px;
+  border: 1px solid red;
+  position: relative;
+  overflow: hidden;
+  .w-carousel-arrow {
+    border: none;
+    outline: none;
+    margin: 0;
+    padding: 0.3em; //去除默认样式
+    border-radius: 50%;
+    background-color: rgba(31, 45, 61, 0.11);
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+    cursor: pointer;
+    svg {
+      fill: white;
+    }
+    &.arrow-left {
+      left: 0.5em;
+    }
+    &.arrow-right {
+      right: 0.5em;
+    }
+  }
+  .w-carousel-wrapper {
+    overflow-x: hidden;
+    min-height: 200px;
+    .w-carousel-item {
       position: absolute;
-      top:50%;
-      transform: translateY(-50%);
-      display: inline-flex;
-      justify-content: center;
-      align-items: center;
-      z-index:1;
-      cursor: pointer;
-      svg{
-        fill:white;
-      }
-      &.arrow-left{
-        left:.5em;
-      }
-      &.arrow-right{
-        right:.5em;
-      }
+      min-width: 100%;
     }
-    .w-carousel-wrapper{
-      // overflow-x: hidden;
-      background:grey;
-      min-height:200px;
-      .w-carousel-item{
-        position:absolute;
-        min-width:100%;
-      }
-    }
-    ul{
-      padding:0;
-      margin:0;
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    li {
       list-style: none;
-      display: flex;
-      justify-content: center;
-      width:100%;
-      position: absolute;
-      bottom:0;
-      li{
-        list-style: none;
-        height: 2px;
-        width:10px;
-        background: white;
-        margin:8px 5px;
-        cursor:pointer;
+      height: 2px;
+      width: 10px;
+      background: white;
+      margin: 8px 5px;
+      cursor: pointer;
+      &.active{
+        background: black;
       }
     }
   }
-
+}
 </style>
